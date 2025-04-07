317ae2ed370475bf76248a25ca01dc91ee88f4ca
its-MAHADi <mhkhandakar57@gmail.com>
2025-04-07 20:39:15 +0600

    module 42 react practice and all setup & data load done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+
+
+  return (
+    <>
+     <Navbar/>
+     <Blogs/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+const Blogs = () => {
+
+  const [blogs,setBlogs] = useState([])
+
+    useEffect(() => {
+        fetch("blogs.json")
+        .then(res => res.json())
+        .then(data => console.log(data))
+    })
+    console.log(blogs)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Blogs
